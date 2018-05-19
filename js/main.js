4cc3cf61c77760381b4f850c107ad3417aaa145c
Mario <marioschantel@gmail.com>
2018-05-19 16:32:06 +0200

    CodeReview 6

---
+++ js/main.js
@@ -0,0 +1,458 @@
+var mediaInfo =[
+	{
+		title: 'Die Insel',
+		type: 'DVD <span class="glyphicon glyphicon-film" aria-hidden="true"></span>',
+		author: 'Michael Bay',
+		genre: 'Drama/Science-Fiction',
+		publisher: 'Warner Bros.',
+		image: 'img/die-insel.jpg',
+		description: '<iframe width="100%" src="https://en.wikipedia.org/wiki/The_Island_(2005_film)" attributes></iframe>' ,
+		rating: '<span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>'
+	},
+
+	{
+		title: 'Eragon',
+		type: 'DVD <span class="glyphicon glyphicon-film" aria-hidden="true"></span>',
+		author: 'Stefen Fangmeier',
+		genre: 'Drama/Fantasy',
+		publisher: '20th Century Fox.',
+		image: 'img/eragon.jpg',
+		description: '<iframe width="100%" src="https://en.wikipedia.org/wiki/Eragon_(film)" attributes></iframe>' ,
+		rating: '<span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>'
+	},
+
+	{
+		title: 'Avatar',
+		type: 'DVD <span class="glyphicon glyphicon-film" aria-hidden="true"></span>',
+		author: 'James Cameron',
+		genre: 'Fantasy/Science-Fiction',
+		publisher: '20th Century Fox.',
+		image: 'img/avatar.jpg',
+		description: '<iframe width="100%" src="https://en.wikipedia.org/wiki/Avatar_(2009_film)" attributes></iframe>' ,
+		rating: '<span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span>'
+	},
+
+	{
+		title: 'X-Men',
+		type: 'DVD <span class="glyphicon glyphicon-film" aria-hidden="true"></span>',
+		author: 'James Mangold',
+		genre: 'Fantasy/Science-Fiction',
+		publisher: '20th Century Fox.',
+		image: 'img/x-men.jpg',
+		description: '<iframe width="100%" src="https://en.wikipedia.org/wiki/X-Men_(film)" attributes></iframe>' ,
+		rating: '<span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>' 
+	},
+
+	{
+		title: 'LOVE?',
+		type: 'CD <span class="glyphicon glyphicon-cd" aria-hidden="true"></span>',
+		author: 'Jennifer Lopez',
+		genre: '',
+		publisher: '',
+		image: 'img/love.jpg',
+		description: '<iframe width="100%" src="https://en.wikipedia.org/wiki/Love%3F" attributes></iframe>' ,
+		rating: '<span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span> <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>'  
+	},
+
+	{
+		title: 'The New Best Of',
+		type: 'CD <span class="glyphicon glyphicon-cd" aria-hidden="true"></span>',
+		author: 'Backstreet Boys',
+		genre: '',
+		publisher: '',
+		image: 'img/bestof.jpg',
+		description: '<iframe width="100%" src="https://en.wikipedia.org/wiki/Backstreet_Boys" attributes></iframe>' ,
+		rating: '<span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>' 
+	},
+
+	{
+		title: 'This Is Take That',
+		type: 'CD <span class="glyphicon glyphicon-cd" aria-hidden="true"></span>',
+		author: 'Take That',
+		genre: '',
+		publisher: '',
+		image: 'img/thisis.jpg',
+		description: '<iframe width="100%" src="https://en.wikipedia.org/wiki/Take_That" attributes></iframe>' ,
+		rating: '<span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>'  
+	},
+
+	{
+		title: 'Rokstarr',
+		type: 'CD <span class="glyphicon glyphicon-cd" aria-hidden="true"></span>',
+		author: 'Taio Cruz',
+		genre: '',
+		publisher: '',
+		image: 'img/rokstarr.jpg',
+		description: '<iframe width="100%" src="https://en.wikipedia.org/wiki/Rokstarr" attributes></iframe>' ,
+		rating: '<span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span>'  
+	},
+
+	{
+		title: 'The Jungle Book',
+		type: 'Book <span class="glyphicon glyphicon-book" aria-hidden="true"></span>',
+		author: 'Rudyard Kipling',
+		genre: '',
+		publisher: '',
+		image: 'img/jungle.jpg',
+		description: '<iframe width="100%" src="https://en.wikipedia.org/wiki/The_Jungle_Book_(1967_film)" attributes></iframe>' ,
+		rating: '<span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span>'  
+	},
+
+	{
+		title: 'TomTurbo - Spaghetti Spuk',
+		type: 'Book <span class="glyphicon glyphicon-book" aria-hidden="true"></span>',
+		author: 'Thomas Brezina',
+		genre: '',
+		publisher: '',
+		image: 'img/tomturbo.jpg',
+		description: '<iframe width="100%" src="https://en.wikipedia.org/wiki/Tom_Turbo" attributes></iframe>' ,
+		rating: '<span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span>'  
+	},
+
+	{
+		title: 'Pinocchio',
+		type: 'Book <span class="glyphicon glyphicon-book" aria-hidden="true"></span>',
+		author: 'Carlo Collodi',
+		genre: '',
+		publisher: '',
+		image: 'img/pinocchio.jpg',
+		description: '<iframe width="100%" src="https://en.wikipedia.org/wiki/Pinocchio_(1940_film)" attributes></iframe>' ,
+		rating: '<span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span>'  
+	},
+
+	{
+		title: 'Beauty & The Beast',
+		type: 'Book <span class="glyphicon glyphicon-book" aria-hidden="true"></span>',
+		author: 'Leprince de Beaumont ',
+		genre: '',
+		publisher: '',
+		image: 'img/beauty.jpg',
+		description: '<iframe width="100%" src="https://en.wikipedia.org/wiki/Beauty_and_the_Beast_(1991_film)" attributes></iframe>' ,
+		rating: '<span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> <span class="glyphicon glyphicon-star" aria-hidden="true"></span>'  
+	},
+
+
+];
+
+//First Media
+
+document.getElementById("mediaTitle1").innerHTML = mediaInfo[0].title;
+
+document.getElementById("mediaType1").innerHTML = mediaInfo[0].type;
+
+document.getElementById("mediaAuthor1").innerHTML = mediaInfo[0].author;
+
+document.getElementById("mediaGenre1").innerHTML = mediaInfo[0].genre;
+
+document.getElementById("mediaPublisher1").innerHTML = mediaInfo[0].publisher;
+
+document.getElementById("mediaImage1").src = mediaInfo[0].image;
+
+//document.getElementById("mediaDescription1").innerHTML = mediaInfo[0].description;
+
+document.getElementById("mediaRating1").innerHTML = mediaInfo[0].rating;
+
+
+//Second Media
+
+document.getElementById("mediaTitle2").innerHTML = mediaInfo[1].title;
+
+document.getElementById("mediaType2").innerHTML = mediaInfo[1].type;
+
+document.getElementById("mediaAuthor2").innerHTML = mediaInfo[1].author;
+
+document.getElementById("mediaGenre2").innerHTML = mediaInfo[1].genre;
+
+document.getElementById("mediaPublisher2").innerHTML = mediaInfo[1].publisher;
+
+document.getElementById("mediaImage2").src = mediaInfo[1].image;
+
+// document.getElementById("mediaDescription2").innerHTML = mediaInfo[1].description;
+2
+document.getElementById("mediaRating2").innerHTML = mediaInfo[1].rating;
+
+//Third Media
+
+document.getElementById("mediaTitle3").innerHTML = mediaInfo[2].title;
+
+document.getElementById("mediaType3").innerHTML = mediaInfo[2].type;
+
+document.getElementById("mediaAuthor3").innerHTML = mediaInfo[2].author;
+
+document.getElementById("mediaGenre3").innerHTML = mediaInfo[2].genre;
+
+document.getElementById("mediaPublisher3").innerHTML = mediaInfo[2].publisher;
+
+document.getElementById("mediaImage3").src = mediaInfo[2].image;
+
+//document.getElementById("mediaDescription3").innerHTML = mediaInfo[2].description;
+
+document.getElementById("mediaRating3").innerHTML = mediaInfo[2].rating;
+
+//Fourth Media
+
+document.getElementById("mediaTitle4").innerHTML = mediaInfo[3].title;
+
+document.getElementById("mediaType4").innerHTML = mediaInfo[3].type;
+
+document.getElementById("mediaAuthor4").innerHTML = mediaInfo[3].author;
+
+document.getElementById("mediaGenre4").innerHTML = mediaInfo[3].genre;
+
+document.getElementById("mediaPublisher4").innerHTML = mediaInfo[3].publisher;
+
+document.getElementById("mediaImage4").src = mediaInfo[3].image;
+
+// document.getElementById("mediaDescription4").innerHTML = mediaInfo[3].description;
+
+document.getElementById("mediaRating4").innerHTML = mediaInfo[3].rating;
+
+//Fifth Media
+
+document.getElementById("mediaTitle5").innerHTML = mediaInfo[4].title;
+
+document.getElementById("mediaType5").innerHTML = mediaInfo[4].type;
+
+document.getElementById("mediaAuthor5").innerHTML = mediaInfo[4].author;
+
+document.getElementById("mediaGenre5").innerHTML = mediaInfo[4].genre;
+
+document.getElementById("mediaPublisher5").innerHTML = mediaInfo[4].publisher;
+
+document.getElementById("mediaImage5").src = mediaInfo[4].image;
+
+// document.getElementById("mediaDescription5").innerHTML = mediaInfo[4].description;
+
+document.getElementById("mediaRating5").innerHTML = mediaInfo[4].rating;
+
+//Sixth Media
+
+document.getElementById("mediaTitle6").innerHTML = mediaInfo[5].title;
+
+document.getElementById("mediaType6").innerHTML = mediaInfo[5].type;
+
+document.getElementById("mediaAuthor6").innerHTML = mediaInfo[5].author;
+
+document.getElementById("mediaGenre6").innerHTML = mediaInfo[5].genre;
+
+document.getElementById("mediaPublisher6").innerHTML = mediaInfo[5].publisher;
+
+document.getElementById("mediaImage6").src = mediaInfo[5].image;
+
+// document.getElementById("mediaDescription6").innerHTML = mediaInfo[5].description;
+
+document.getElementById("mediaRating6").innerHTML = mediaInfo[5].rating;
+
+//Seventh Media
+
+document.getElementById("mediaTitle7").innerHTML = mediaInfo[6].title;
+
+document.getElementById("mediaType7").innerHTML = mediaInfo[6].type;
+
+document.getElementById("mediaAuthor7").innerHTML = mediaInfo[6].author;
+
+document.getElementById("mediaGenre7").innerHTML = mediaInfo[6].genre;
+
+document.getElementById("mediaPublisher7").innerHTML = mediaInfo[6].publisher;
+
+document.getElementById("mediaImage7").src = mediaInfo[6].image;
+
+// document.getElementById("mediaDescription7").innerHTML = mediaInfo[6].description;
+
+document.getElementById("mediaRating7").innerHTML = mediaInfo[6].rating;
+
+//Eighth Media
+
+document.getElementById("mediaTitle8").innerHTML = mediaInfo[7].title;
+
+document.getElementById("mediaType8").innerHTML = mediaInfo[7].type;
+
+document.getElementById("mediaAuthor8").innerHTML = mediaInfo[7].author;
+
+document.getElementById("mediaGenre8").innerHTML = mediaInfo[7].genre;
+
+document.getElementById("mediaPublisher8").innerHTML = mediaInfo[7].publisher;
+
+document.getElementById("mediaImage8").src = mediaInfo[7].image;
+
+// document.getElementById("mediaDescription8").innerHTML = mediaInfo[7].description;
+
+document.getElementById("mediaRating8").innerHTML = mediaInfo[7].rating;
+
+//Nineth Media
+
+document.getElementById("mediaTitle9").innerHTML = mediaInfo[8].title;
+
+document.getElementById("mediaType9").innerHTML = mediaInfo[8].type;
+
+document.getElementById("mediaAuthor9").innerHTML = mediaInfo[8].author;
+
+document.getElementById("mediaGenre9").innerHTML = mediaInfo[8].genre;
+
+document.getElementById("mediaPublisher9").innerHTML = mediaInfo[8].publisher;
+
+document.getElementById("mediaImage9").src = mediaInfo[8].image;
+
+// document.getElementById("mediaDescription9").innerHTML = mediaInfo[8].description;
+
+document.getElementById("mediaRating9").innerHTML = mediaInfo[8].rating;
+
+//Tenth Media
+
+document.getElementById("mediaTitle10").innerHTML = mediaInfo[9].title;
+
+document.getElementById("mediaType10").innerHTML = mediaInfo[9].type;
+
+document.getElementById("mediaAuthor10").innerHTML = mediaInfo[9].author;
+
+document.getElementById("mediaGenre10").innerHTML = mediaInfo[9].genre;
+
+document.getElementById("mediaPublisher10").innerHTML = mediaInfo[9].publisher;
+
+document.getElementById("mediaImage10").src = mediaInfo[9].image;
+
+// document.getElementById("mediaDescription10").innerHTML = mediaInfo[9].description;
+
+document.getElementById("mediaRating10").innerHTML = mediaInfo[9].rating;
+
+//Eleventh Media10
+
+document.getElementById("mediaTitle11").innerHTML = mediaInfo[10].title;
+
+document.getElementById("mediaType11").innerHTML = mediaInfo[10].type;
+
+document.getElementById("mediaAuthor11").innerHTML = mediaInfo[10].author;
+
+document.getElementById("mediaGenre11").innerHTML = mediaInfo[10].genre;
+
+document.getElementById("mediaPublisher11").innerHTML = mediaInfo[10].publisher;
+
+document.getElementById("mediaImage11").src = mediaInfo[10].image;
+
+// document.getElementById("mediaDescription11").innerHTML = mediaInfo[10].description;
+
+document.getElementById("mediaRating11").innerHTML = mediaInfo[10].rating;
+
+//Twelveth Media
+
+document.getElementById("mediaTitle12").innerHTML = mediaInfo[11].title;
+
+document.getElementById("mediaType12").innerHTML = mediaInfo[11].type;
+
+document.getElementById("mediaAuthor12").innerHTML = mediaInfo[11].author;
+
+document.getElementById("mediaGenre12").innerHTML = mediaInfo[11].genre;
+
+document.getElementById("mediaPublisher12").innerHTML = mediaInfo[11].publisher;
+
+document.getElementById("mediaImage12").src = mediaInfo[11].image;
+
+// document.getElementById("mediaDescription12").innerHTML = mediaInfo[11].description;
+
+document.getElementById("mediaRating12").innerHTML = mediaInfo[11].rating;
+
+
+//---- MODAL ----
+
+document.getElementById("modalTitle1").innerHTML = mediaInfo[0].title;
+
+document.getElementById("modalContent1").innerHTML = mediaInfo[0].description;
+
+document.getElementById("modalTitle2").innerHTML = mediaInfo[1].title;
+
+document.getElementById("modalContent2").innerHTML = mediaInfo[1].description;
+
+document.getElementById("modalTitle3").innerHTML = mediaInfo[2].title;
+
+document.getElementById("modalContent3").innerHTML = mediaInfo[2].description;
+
+document.getElementById("modalTitle4").innerHTML = mediaInfo[3].title;
+
+document.getElementById("modalContent4").innerHTML = mediaInfo[3].description;
+
+document.getElementById("modalTitle5").innerHTML = mediaInfo[4].title;
+
+document.getElementById("modalContent5").innerHTML = mediaInfo[4].description;
+
+document.getElementById("modalTitle6").innerHTML = mediaInfo[5].title;
+
+document.getElementById("modalContent6").innerHTML = mediaInfo[5].description;
+
+document.getElementById("modalTitle7").innerHTML = mediaInfo[6].title;
+
+document.getElementById("modalContent7").innerHTML = mediaInfo[6].description;
+
+document.getElementById("modalTitle8").innerHTML = mediaInfo[7].title;
+
+document.getElementById("modalContent8").innerHTML = mediaInfo[7].description;
+
+document.getElementById("modalTitle9").innerHTML = mediaInfo[8].title;
+
+document.getElementById("modalContent9").innerHTML = mediaInfo[8].description;
+
+document.getElementById("modalTitle10").innerHTML = mediaInfo[9].title;
+
+document.getElementById("modalContent10").innerHTML = mediaInfo[9].description;
+
+document.getElementById("modalTitle11").innerHTML = mediaInfo[10].title;
+
+document.getElementById("modalContent11").innerHTML = mediaInfo[10].description;
+
+document.getElementById("modalTitle12").innerHTML = mediaInfo[11].title;
+
+document.getElementById("modalContent12").innerHTML = mediaInfo[11].description;
+
+//--- STAR RATING [NOT WORKING] ---
+
+// initialize with defaults
+//$("#mediaRating1").rating();
+
+// --- LOOP [NOT WORKING] ---
+/*
+//MEDIA INFO GENERATION
+// Media Info Holder
+var mediaInfo = ' ';
+//Titles
+var mediaTitle = ['Die Insel', 'Eragon', 'Avatar', 'X-Men'];
+//Types (CD, Book, DVD, Comic)
+var mediaType = ['DVD', 'DVD', 'DVD', 'DVD']
+//Author
+var mediaAuthor = ['Michael Bay', 'Stefen Fangmeier', 'James Cameron', 'James Mangold'];
+//Genre
+var mediaGenre = ['Drama/Science-Fiction', 'Drama/Fantasy', 'Fantasy/Science-Fiction', 'Fantasy/Science-Fiction'];
+//Publisher
+var mediaPublisher = ['Warner Bros.', '20th Century Fox.', '20th Century Fox', '20th Century Fox'];
+//Image
+var mediaImage = ['die-insel', 'eragon', 'avatar', 'x-men'];
+//Short Description
+var mediaDescription = ['Description Insel', 'Description Eragon', 'Description Avatar', 'Description X-Men'];
+//Rating (1-5)
+var mediaRating = ['4', '4', '5', '5'];
+
+//Generate
+for (var i = 0; i < mediaInfo.length; i++) {
+    var title = mediaTitle[i];
+    var type = mediaType[i];
+    var author = mediaAuthor[i];
+    var genre = mediaGenre [i];
+    var publisher = mediaPublisher[i];
+    var image = mediaImage [i];
+    var description = mediaDescription[i];
+    var rating = mediaRating[i];
+
+    mediaInfo += "<figure class='col-md-3 green'>" +
+				"<img class='img-responsive' src='img/" + image + "alt=''>" +
+				"<figcaption>" +
+					"<h4><span class='mediaTitle'>" +
+					title + "</span></h4>" +
+					"by <h5><span class='mediaAuthor'>" +
+					author + "</span></h5>" +
+
+				"</figcaption>" +
+			"</figure>"
+		}
+
+$("#mediaThek").html(mediaInfo);
+
+
+*/
